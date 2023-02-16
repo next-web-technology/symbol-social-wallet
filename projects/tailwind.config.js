50b435db969fb055ffcb684cba85f2bd50cb1374
kobakaku <kobakaku1616@gmail.com>
2023-02-16 15:01:44 +0900

    do'nt display tailwind

---
+++ projects/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./web/src/**/*.{html,ts}', './admin/src/**/*.{html,ts}', './lp/src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
