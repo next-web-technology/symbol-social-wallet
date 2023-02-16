1a451fa1a59084dcf9eb26fd2bbf94ebfb40f343
kobakaku <kobakaku1616@gmail.com>
2023-02-16 15:38:29 +0900

    do'nt recognize @tailwind directorive in case use scss file

--- projects/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./web/src/**/*.{html,ts}', './admin/src/**/*.{html,ts}', './lp/src/**/*.{html,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
